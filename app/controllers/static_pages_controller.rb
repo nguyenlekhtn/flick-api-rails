903891f98b042f1822fba741a8fd5db5d23dbbce
nguyenlekhtn <25528200+nguyenlekhtn@users.noreply.github.com>
2023-05-02 09:27:14 +0700

    add style for photo grid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
     flickr = Flickr.new
     user_id = params[:user_id]
     if user_id
-      @photo_urls = flickr.photos.search(user_id:).map { |photo| {url: Flickr.url(photo), url_photopage: Flickr.url_photopage(photo)} }
+      @photo_urls = flickr.photos.search(user_id:, media: "photos").map { |photo| {url: Flickr.url(photo), url_photopage: Flickr.url_photopage(photo)} }
     end
   end
 end
